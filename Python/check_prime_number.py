c5f5822f4c6bbd67c129f9de838133aca349cd0d
yerminal <60397218+yerminal@users.noreply.github.com>
2020-12-06 16:34:52 +0300

    Update check_prime_number.py

--- Python/check_prime_number.py
+++ Python/check_prime_number.py
@@ -1,5 +1,5 @@
 def check_prime_number(number):
     if((number%2==0 and number!=2) or number==1): return False
-    for i in range(3,int((number/2)+1),2):
+    for i in range(3,number//2),2):
         if(number%i == 0): return False
     return True
