ba9b7fa424b84d248a2b2d800fc44b59bf1f1304
yerminal <60397218+yerminal@users.noreply.github.com>
2021-01-26 01:09:07 +0300

    Update mode_of_series.py

--- Python/mode_of_series.py
+++ Python/mode_of_series.py
@@ -1,11 +1,25 @@
-def mode_dict(lst):
+def mode_dict(lst): # It returns a dict that contains frequencies of the input.
+    """
+    >>> mode_dict([3,3,4,4,5,5,6])
+    {3: 2, 4: 2, 5: 2, 6: 1}
+    >>> mode_dict(["x","y","x"])
+    {'x': 2, 'y': 1}
+    """
     unique_values = {i: 0 for i in list(set(lst))}
     for i in lst:
         if i in unique_values.keys():
             unique_values[i] += 1
     return unique_values
 
-def mode(lst):
+def mode(lst): # It returns all modes of the input.
+    """
+    >>> mode(["x","y","x"])
+    ['x']
+    >>> mode(["x","y","x","y","z"])
+    ['x', 'y']
+    >>> mode([3,3,4,4,5,5,6])
+    [3, 4, 5] 
+    """
     unique_values = {i: 0 for i in list(set(lst))}
     for i in lst:
         if i in unique_values.keys():
